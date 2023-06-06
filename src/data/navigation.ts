57569a3a5f368639456f453b9ab622b94dbe1607
Kunal Keshan <kunalkeshan12@gmail.com>
2023-06-06 14:50:48 +0530

    Feat/Navbar (#20)
    * feat: navbar completed
    
    * chore: navbar documentation added

---
+++ src/data/navigation.ts
@@ -0,0 +1,16 @@
+// Navigation Data
+
+const COMMON_LINKS = [
+	{
+		name: 'Home',
+		url: '/',
+	},
+	{
+		name: 'Features',
+		url: '/#features',
+	},
+];
+
+export const MAIN_LINKS = [...COMMON_LINKS];
+
+export const FOOTER_LINKS = [...COMMON_LINKS];
