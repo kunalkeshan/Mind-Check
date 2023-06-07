0e03661a6bb5b03924b331978b87d206741b64cd
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-08 00:35:00 +0530

    chore: test page - instructions added

---
+++ src/components/test/Instructions.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import INSTRUCTIONS from '../../data/instructions';
+
+interface InstructionsProps {
+	setTestInProgress: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Instructions: React.FC<InstructionsProps> = ({ setTestInProgress }) => {
+	const handleStartTest = () => {
+		setTestInProgress(true);
+	};
+
+	return (
+		<section className='w-full max-w-7xl mx-auto py-12 px-8 flex flex-col'>
+			<h1 className='font-heading text-3xl md:text-5xl font-bold text-center'>
+				Mental Health Check
+			</h1>
+			<h2 className='text-lg md:text-2xl font-medium text-center max-w-2xl mx-auto mt-4'>
+				Assess your well-being with a comprehensive 25-question test.
+				Based on the Burns Depression Checklist from Feeling Good: the
+				new mood therapy.
+			</h2>
+			<div className='flex flex-col gap-8 mt-8 w-full'>
+				{INSTRUCTIONS.map((instruction, index) => (
+					<div
+						key={index}
+						className={`${
+							index % 2
+								? index % 3
+									? 'bg-secondary'
+									: 'bg-secondaryDark'
+								: 'bg-tertiary'
+						} w-full rounded-xl text-textPrimary grid grid-cols-1 md:grid-cols-2 py-8 px-12 place-items-center sticky shadow hover:-translate-y-1 hover:shadow-md transition-all`}
+						style={{
+							top: `${(index + 1) * 2}em`,
+						}}
+					>
+						<div>
+							<p className='font-heading font-bold text-base md:text-2xl'>
+								{instruction.text}
+							</p>
+						</div>
+						<div className='hidden md:block'>
+							<div className='max-w-xs rounded-full overflow-hidden'>
+								<img
+									src={instruction.image}
+									alt={instruction.text}
+									className='w-full h-auto object-contain'
+								/>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+			<div className='min-h-[20vh] md:min-h-[40vh] mt-12 flex flex-col items-center justify-center gap-8 text-center'>
+				<h2 className='text-3xl md:text-6xl max-w-3xl font-heading'>
+					Don't wait anymore to start taking control of your mind and
+					your life.
+				</h2>
+				<button
+					onClick={handleStartTest}
+					className='px-8 py-4 mx-auto border-secondary border-2 rounded-full font-semibold hover:bg-tertiary transition-all hover:border-secondaryDark'
+				>
+					Take test now
+				</button>
+			</div>
+		</section>
+	);
+};
+
+export default Instructions;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import Instructions from '../components/test/Instructions';
+import { Link } from 'react-router-dom';
+
 function Test() {
-	return <div>Test</div>;
+	const [testInProgress, setTestInProgress] = useState(false);
+	return (
+		<>
+			<nav className='fixed top-0 left-0 py-4 px-8 z-50 bg-primary w-full text-textPrimary'>
+				<div className='max-w-7xl mx-auto flex items-center justify-between md:justify-normal w-full md:gap-8'>
+					<Link
+						to={'/'}
+						className='flex w-fit items-center gap-1 hover:scale-[0.98] active:scale-[1.02] transition-all'
+					>
+						<div className='w-10 h-10'>
+							<img
+								src='/mind-check-logo.png'
+								alt='🧠'
+								className='w-full h-auto object-contain'
+							/>
+						</div>
+						<p className='font-heading text-3xl font-bold'>
+							Mind Check
+						</p>
+					</Link>
+				</div>
+			</nav>
+			<main className='w-full min-h-screen mt-12'>
+				{testInProgress ? (
+					<></>
+				) : (
+					<Instructions setTestInProgress={setTestInProgress} />
+				)}
+			</main>
+		</>
+	);
 }
 
 export default Test;
