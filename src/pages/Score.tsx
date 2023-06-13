cf909dbee38529e26272a3ae01ea0fe4c109b7d1
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-13 15:36:21 +0530

    chore: score page ui completed

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,5 +1,83 @@
+import { useMemo, useLayoutEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 function Score() {
-	return <div>Score</div>;
+	const navigate = useNavigate();
+
+	const score = useMemo(() => {
+		if (sessionStorage.getItem('MindCheckUserScore.v1')) {
+			return parseInt(
+				sessionStorage.getItem('MindCheckUserScore.v1') as string,
+				10
+			);
+		} else return null;
+	}, []);
+
+	useLayoutEffect(() => {
+		if (!score) navigate('/test');
+	}, [navigate, score]);
+
+	return (
+		<>
+			<nav className='fixed top-0 left-0 py-4 px-8 z-50 bg-primary w-full text-textPrimary'>
+				<div className='max-w-7xl mx-auto flex items-center justify-between md:justify-normal w-full md:gap-8'>
+					<Link
+						to={'/'}
+						className='flex w-fit items-center gap-1 hover:scale-[0.98] active:scale-[1.02] transition-all'
+					>
+						<div className='w-10 h-10'>
+							<img
+								src='/mind-check-logo.png'
+								alt='🧠'
+								className='w-full h-auto object-contain'
+							/>
+						</div>
+						<p className='font-heading text-3xl font-bold'>
+							Mind Check
+						</p>
+					</Link>
+				</div>
+			</nav>
+			<main className='w-full max-w-7xl mx-auto py-12 px-8 flex flex-col mt-12'>
+				<h1 className='font-heading text-3xl md:text-5xl font-bold text-center'>
+					Your Mental Health Score
+				</h1>
+				<h2 className='text-lg md:text-2xl font-medium text-center max-w-2xl mx-auto mt-4'>
+					Discover your mental well-being with a personalized score
+					and helpful feedback.
+				</h2>
+
+				<section className='flex flex-col items-center text-center gap-6 max-w-2xl mx-auto mt-6'>
+					<div className='w-40 h-40 border-8 border-green-500 text-green-500 text-4xl flex items-center justify-center font-bold font-heading rounded-full'>
+						5/100
+					</div>
+					<p className='text-xl font-semibold'>
+						Great job! Your score suggests that you are currently
+						experiencing no symptoms of depression. Keep up the
+						positive outlook and continue taking care of your mental
+						well-being.
+					</p>
+					<p>
+						<b>Note:</b> If you experience persistent scores above
+						10 or have any suicidal feelings, it's important to seek
+						professional treatment and consult with a mental health
+						professional immediately.
+					</p>
+					<p>
+						Remember, the app is designed to provide guidance and
+						support, but it's not a substitute for professional
+						help.
+					</p>
+					<Link
+						to='/test'
+						className='px-8 py-4 mx-auto border-secondary border-2 mt-4 w-full rounded-full font-semibold hover:bg-tertiary transition-all hover:border-secondaryDark'
+					>
+						Take another test.
+					</Link>
+				</section>
+			</main>
+		</>
+	);
 }
 
 export default Score;
