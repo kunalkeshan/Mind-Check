4f3ca0212f45ef7633a9546cd470ed5d3de42d97
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-16 15:12:27 +0530

    chore: added work in progress component for login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import PublicLayout from '../components/layout/PublicLayout';
+import WorkInProgress from '../components/reusable/WorkInProgress';
+
+function Login() {
+	return (
+		<PublicLayout>
+			<WorkInProgress />
+		</PublicLayout>
+	);
+}
+
+export default Login;
