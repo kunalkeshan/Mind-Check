c6ad7d5b7c02eabea722f67e0a565e6ff961998e
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-25 08:41:48 +0530

    chore: using react hot toast to handle errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AppContext.tsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
 				<App />
+				<Toaster position='bottom-right' />
 			</AuthProvider>
 		</BrowserRouter>
 	</React.StrictMode>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,10 +12,13 @@ import { useUserStore } from '../store/user';
 import { FirebaseAuth, FirebaseDb } from '../firebase';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
+import { FirebaseError } from 'firebase/app';
+import { toast } from 'react-hot-toast';
 
 function Login() {
 	const [loading, setLoading] = useState(false);
 	const { setUser } = useUserStore();
+
 	const handleLoginWithGoogle = async () => {
 		const provider = new GoogleAuthProvider();
 		try {
@@ -27,16 +30,25 @@ function Login() {
 				response.user.metadata.lastSignInTime
 			) {
 				// Is New User
-				const scoreDocRef = doc(
-					FirebaseDb,
-					'scores',
-					response.user.uid
-				);
-				setDoc(scoreDocRef, { scores: [] });
+				const scoreDocRef = doc(FirebaseDb, 'users', response.user.uid);
+				setDoc(scoreDocRef, {});
 			}
 			setUser(response.user);
 		} catch (error) {
-			console.log(error);
+			if (error instanceof FirebaseError) {
+				switch (error.code) {
+					case 'auth/popup-closed-by-user': {
+						toast.error(
+							'Do not close popup before selecting email.'
+						);
+						break;
+					}
+					default: {
+						toast.error('Unable to login. Try again later.');
+						break;
+					}
+				}
+			}
 		} finally {
 			setLoading(false);
 		}
@@ -44,36 +56,37 @@ function Login() {
 
 	return (
 		<PublicLayout>
-			<div className='grid grid-cols-1 md:grid-cols-2 text-center md:text-left'>
-				<section>
-					<div className='w-11/12'>
-						<Lottie
-							animationData={LoginAnimationData}
-							loop={true}
+			<div className='text-center max-w-3xl mx-auto flex flex-col justify-center'>
+				<h1 className='font-heading text-3xl md:text-5xl font-bold'>
+					Access your Account
+				</h1>
+				<button
+					onClick={handleLoginWithGoogle}
+					className={`${
+						loading
+							? 'grayscale'
+							: 'hover:border-secondaryDark hover:bg-tertiary'
+					} px-8 py-4 w-full mt-8 text-base md:text-2xl justify-center gap-8 flex items-center border-secondary border-2 rounded-full font-semibold  transition-all `}
+					disabled={loading}
+				>
+					<span className='w-8 md:w-10 h-8 md:h-10'>
+						<img
+							src='/icons/google.png'
+							alt='Google'
 							className='w-full h-auto object-contain'
 						/>
-					</div>
-				</section>
-				<section>
-					<h1 className='font-heading text-3xl md:text-5xl font-bold'>
-						Access your Account
-					</h1>
-					<button
-						onClick={handleLoginWithGoogle}
-						className='px-8 py-4 w-full mt-8 text-base md:text-2xl justify-center gap-8 flex items-center border-secondary border-2 rounded-full font-semibold hover:bg-tertiary transition-all hover:border-secondaryDark'
-					>
-						<span className='w-8 md:w-10 h-8 md:h-10'>
-							<img
-								src='/icons/google.png'
-								alt='Google'
-								className='w-full h-auto object-contain'
-							/>
-						</span>
-						<span className='w-full md:w-fit'>
-							Continue with Google
-						</span>
-					</button>
-				</section>
+					</span>
+					<span className='w-full md:w-fit'>
+						{loading ? 'Loading...' : 'Continue with Google'}
+					</span>
+				</button>
+				<div className='w-3/4 mx-auto mt-8'>
+					<Lottie
+						animationData={LoginAnimationData}
+						loop={true}
+						className='w-full h-auto object-contain'
+					/>
+				</div>
 			</div>
 			<ScrollToTop />
 		</PublicLayout>
