8a4516e741c7addfbbb6207497e899d4a98d1b66
kunalkeshan <kunalkeshan12@gmail.com>
2023-06-28 09:36:14 +0530

    chore: organized user profile components and routes

---
+++ src/pages/me/Profile.tsx
@@ -0,0 +1,16 @@
+/**
+ * Profile Page
+ */
+
+// Dependencies
+import WorkInProgress from '../../components/reusable/WorkInProgress';
+
+function Profile() {
+	return (
+		<div>
+			<WorkInProgress />
+		</div>
+	);
+}
+
+export default Profile;
---
+++ src/pages/me/ScoreHistory.tsx
@@ -0,0 +1,16 @@
+/**
+ * Score History Page
+ */
+
+// Dependencies
+import WorkInProgress from '../../components/reusable/WorkInProgress';
+
+function ScoreHistory() {
+	return (
+		<div>
+			<WorkInProgress />
+		</div>
+	);
+}
+
+export default ScoreHistory;
