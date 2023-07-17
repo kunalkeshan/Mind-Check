926a542d5f508f557815c6ea2cfb1b4c1cf96da5
kunalkeshan <kunalkeshan12@gmail.com>
2023-07-17 09:45:58 +0530

    chore: switch to session & promise all for resources

--- src/utils/resources.ts
+++ src/utils/resources.ts
@@ -3,37 +3,37 @@ import RESOURCES, { Resource } from '../data/resources';
 const SOURCE_URL = '/resources';
 
 export const fetchAllResources = async () => {
-	const resources: Resource[] = [];
-	RESOURCES.forEach(async (resource) => {
-		if (localStorage.getItem(resource.url)) {
+	const data = RESOURCES.map(async (resource) => {
+		if (sessionStorage.getItem(resource.url)) {
 			const data = JSON.parse(
-				localStorage.getItem(resource.url) as string
+				sessionStorage.getItem(resource.url) as string
 			) as Resource;
-			resources.push(data);
+			return data;
 		} else {
 			const response = await fetch(`${SOURCE_URL}/${resource.url}.md`);
 			const text = await response.text();
 			const data = { ...resource, body: text } as Resource;
-			localStorage.setItem(resource.url, JSON.stringify(data));
-			resources.push(data);
+			sessionStorage.setItem(resource.url, JSON.stringify(data));
+			return data;
 		}
 	});
+	const resources = await Promise.all(data);
 	return resources;
 };
 
 export const fetchSingleResource = async (url: string) => {
 	const resource = RESOURCES.find((res) => res.url === url);
 	if (!resource) throw new Error('Given URL is invalid.');
-	if (localStorage.getItem(url)) {
+	if (sessionStorage.getItem(url)) {
 		const data = JSON.parse(
-			localStorage.getItem(url) as string
+			sessionStorage.getItem(url) as string
 		) as Resource;
 		return data;
 	} else {
 		const response = await fetch(`${SOURCE_URL}/${url}.md`);
 		const text = await response.text();
 		const data = { ...resource, body: text } as Resource;
-		localStorage.setItem(resource.url, JSON.stringify(data));
+		sessionStorage.setItem(resource.url, JSON.stringify(data));
 		return data;
 	}
 };
