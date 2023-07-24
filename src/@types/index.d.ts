fe2982d6d86942872f155c8974bba008fb1e1547
kunalkeshan <kunalkeshan12@gmail.com>
2023-07-24 20:24:44 +0530

    feat: recommended reads added
    - to score page
    - to individual score page

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,13 @@
-interface UserTestScores {
+type ScoreValue = Record<number, number>;
+
+interface Score {
 	id: string;
+	calculatedScore: number;
+	time: string;
+	score: {
+		'Thoughts & Feelings': ScoreValue;
+		'Activities & Personal Relationships': ScoreValue;
+		'Physical Symptoms': ScoreValue;
+		'Suicidal Urges': ScoreValue;
+	};
 }
